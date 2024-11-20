e2fde7e1e3e25093ab11834d75db66d38cbe8892
Nandakishore <nmnandakishore@gmail.com>
2024-11-20 14:08:46 +0530

    implemented caching

--- app/Services/NewsService.php
+++ app/Services/NewsService.php
@@ -6,6 +6,7 @@ use App\Models\News;
 use App\Models\User;
 use Carbon\Carbon;
 use Exception;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 
 class NewsService
@@ -13,7 +14,7 @@ class NewsService
     private News $news;
     private UserService $userService;
 
-    public function __construct(News $news, UserService  $userService)
+    public function __construct(News $news, UserService $userService)
     {
         $this->news = $news;
         $this->userService = $userService;
@@ -29,13 +30,13 @@ class NewsService
     {
         try {
             return $this->news->where('id', $id)->first()->toArray();
-        } catch (Exception $exception){
+        } catch (Exception $exception) {
             throw $exception;
         }
     }
 
     /**
-     * Paginated list of all news
+     * Cached and paginated list of all news
      * @param int|null $page
      * @param int|null $pageSize
      * @return array
@@ -44,21 +45,24 @@ class NewsService
     public function listAll(int $page = null, int $pageSize = null): array
     {
         try {
-            return $this->news
-                ->paginate($pageSize ?? 10, ['*'], 'page', $page ?? 1)
-                ->appends(['pageSize' => $pageSize ?? 10])
-                ->toArray();
+            return Cache::remember('listAll' . $page . $pageSize, 300, function () use ($page, $pageSize) {
+                return $this->news
+                    ->paginate($pageSize ?? 10, ['*'], 'page', $page ?? 1)
+                    ->appends(['pageSize' => $pageSize ?? 10])
+                    ->toArray();
+            });
         } catch (Exception $exception) {
             throw $exception;
         }
     }
 
     /**
-     * Paginated list of news filtered with keyword, date, category or source
-     * @param $keyword
-     * @param $date
-     * @param $category
-     * @param $source
+     * Cached and paginated list of news filtered with keyword, date, category or source
+     * @param null $keyword
+     * @param null $date
+     * @param array|null $category
+     * @param array|null $source
+     * @param array|null $author
      * @param int|null $page
      * @param int|null $pageSize
      * @return array
@@ -67,34 +71,40 @@ class NewsService
     public function filter($keyword = null, $date = null, array $category = null, array $source = null, array $author = null, int $page = null, int $pageSize = null): array
     {
         try {
-            dump($source);
-            $news = $this->news;
-            $searchKey = '%' . $keyword . '%';
+            $categoryForCacheName = ($category && sizeof($category)) ? implode(',', $category) : "";
+            $sourceForCacheName = ($source && sizeof($source)) ? implode(',', $source) : "";
+            $authorForCacheName = ($author && sizeof($author)) ? implode(',', $author) : "";
+            $cacheName = 'filter' . $keyword . $date . $categoryForCacheName . $sourceForCacheName . $authorForCacheName . $page . $pageSize;
 
-            if ($keyword) {
-                $news = $news
-                    ->where('title', 'like', $searchKey)
-                    ->orWhere('description', 'like', $searchKey)
-                    ->orWhere('content', 'like', $searchKey)
-                    ->orWhere('category', 'like', $searchKey);
-            }
-            if ($date) {
-                $news = $news->whereDate('published_at', Carbon::createFromFormat('d-m-Y', $date));
-            }
-            if ($category) {
-                $news = $news->whereIn('category', $category);
-            }
-            if ($source) {
-                $news = $news->whereIn('source', $source);
-            }
-            if ($author) {
-                $news = $news->whereIn('$author', $source);
-            }
+            return Cache::remember($cacheName, 300, function () use ($keyword, $date, $category, $source, $author, $page, $pageSize) {
+                $news = $this->news;
+                $searchKey = '%' . $keyword . '%';
 
-            return $news
-                ->paginate($pageSize ?? 10, ['*'], 'page', $page ?? 1)
-                ->appends(['pageSize' => $pageSize ?? 10])
-                ->toArray();
+                if ($keyword) {
+                    $news = $news
+                        ->where('title', 'ilike', $searchKey)
+                        ->orWhere('description', 'ilike', $searchKey)
+                        ->orWhere('content', 'ilike', $searchKey)
+                        ->orWhere('category', 'ilike', $searchKey);
+                }
+                if ($date) {
+                    $news = $news->whereDate('published_at', Carbon::createFromFormat('d-m-Y', $date));
+                }
+                if ($category) {
+                    $news = $news->whereIn('category', $category);
+                }
+                if ($source) {
+                    $news = $news->whereIn('source', $source);
+                }
+                if ($author) {
+                    $news = $news->whereIn('$author', $source);
+                }
+
+                return $news
+                    ->paginate($pageSize ?? 10, ['*'], 'page', $page ?? 1)
+                    ->appends(['pageSize' => $pageSize ?? 10])
+                    ->toArray();
+            });
 
         } catch (Exception $exception) {
             throw $exception;
@@ -102,7 +112,7 @@ class NewsService
     }
 
     /**
-     * List news using users personalized filters
+     * Cached list of news using users personalized filters
      * @param User $user
      * @param int|null $page
      * @param int|null $pageSize
@@ -112,48 +122,60 @@ class NewsService
     public function listPersonalized(User $user, int $page = null, int $pageSize = null): array
     {
         try {
-            $preferences = collect($this->userService->listPreferences($user));
+            $cacheName = 'filter' . $user->id . $page . $pageSize;
+            var_dump($cacheName);
 
-            $preferredCategories = $preferences->where('preference_name', 'categories')->first();
-            $preferredAuthors = $preferences->where('preference_name', 'authors')->first();
-            $preferredSources = $preferences->where('preference_name', 'sources')->first();
+            return Cache::remember($cacheName, 300, function () use ($user, $page, $pageSize) {
 
-            $news = $this->news;
-            if ($preferredCategories) {
-                $news = $news->OrWhereIn('category', json_decode($preferredCategories['value']));
-            }
-            if ($preferredSources) {
-                $news = $news->OrWhereIn('source', json_decode($preferredSources['value']));
-            }
-            if ($preferredAuthors) {
-                $news = $news->OrWhereIn('author', (json_decode($preferredAuthors['value'])));
-            }
+                $preferences = collect($this->userService->listPreferences($user));
+                $preferredCategories = $preferences->where('preference_name', 'categories')->first();
+                $preferredAuthors = $preferences->where('preference_name', 'authors')->first();
+                $preferredSources = $preferences->where('preference_name', 'sources')->first();
 
-            $result = $news
-                ->paginate($pageSize ?? 10, ['*'], 'page', $page ?? 1)
-                ->appends(['pageSize' => $pageSize ?? 10])
-                ->toArray();
+                $news = $this->news;
+                if ($preferredCategories) {
+                    $news = $news->OrWhereIn('category', json_decode($preferredCategories['value']));
+                }
+                if ($preferredSources) {
+                    $news = $news->OrWhereIn('source', json_decode($preferredSources['value']));
+                }
+                if ($preferredAuthors) {
+                    $news = $news->OrWhereIn('author', (json_decode($preferredAuthors['value'])));
+                }
 
-            return $result;
-        } catch (Exception $exception){
+                $result = $news
+                    ->paginate($pageSize ?? 10, ['*'], 'page', $page ?? 1)
+                    ->appends(['pageSize' => $pageSize ?? 10])
+                    ->toArray();
+
+                return $result;
+            });
+        } catch (Exception $exception) {
             throw $exception;
         }
     }
 
+    /**
+     * Cached list of all available filters
+     * @return array
+     * @throws Exception
+     */
     public function getFilters(): array
     {
         try {
-            $filters = $this->news
-                ->select('category', 'source', 'author')
-                ->distinct()
-                ->get();
+            return Cache::remember("filters", 300, function () {
+                $filters = $this->news
+                    ->select('category', 'source', 'author')
+                    ->distinct()
+                    ->get();
 
-            return [
-                'categories' => array_values($filters->pluck('category')->unique()->toArray()),
-                'source' => array_values($filters->pluck('source')->unique()->toArray()),
-                'author' => array_values($filters->pluck('author')->unique()->toArray()),
-            ];
-        } catch (Exception $exception){
+                return [
+                    'categories' => array_values($filters->pluck('category')->unique()->toArray()),
+                    'source' => array_values($filters->pluck('source')->unique()->toArray()),
+                    'author' => array_values($filters->pluck('author')->unique()->toArray()),
+                ];
+            });
+        } catch (Exception $exception) {
             throw $exception;
         }
     }
